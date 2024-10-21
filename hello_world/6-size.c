d9fb056ab495fc5e2cdc7f472f8b01d5c8083f3f
Etiennepev <etienneloichot@gmail.com>
2024-10-21 12:03:31 +0000

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	printf("Size of a char: %zu bite(s)\n", sizeof(char));
-	printf("Size of a int: %zu bite(s)\n", sizeof(int));
+	printf("Size of an int: %zu bite(s)\n", sizeof(int));
 	printf("Size of a long int: %zu bite(s)\n",sizeof(long int));
 	printf("Size of a long long int: %zu bite(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu bite(s)\n", sizeof(float));
