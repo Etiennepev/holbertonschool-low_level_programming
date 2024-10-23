61e128af258c2b2d0abf9dccde47f788f5562b18
Etiennepev <etienneloichot@gmail.com>
2024-10-23 14:07:24 +0000

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,18 +13,19 @@ void jack_bauer(void)
 for (heures = 0; heures < 24; heures++)
 {
 for (minutes = 0; minutes < 59; minutes++)
-	{
+{
 int a = (heures / 10);
 int b = (heures % 10);
 int c = (minutes / 10);
 int d = (minutes % 10);
-{
+
 	_putchar(a + '0');
 	_putchar(b + '0');
-	_putchar(':')
+	_putchar(':');
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
 }
 }
 }
+
