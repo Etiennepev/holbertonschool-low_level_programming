ff6ae978716bc5fe1c9bcd2aaa429987ab0cfe99
Etiennepev <etienneloichot@gmail.com>
2024-10-23 11:32:52 +0000

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_abs - prend un entier comme argument et renvoie
+*sa valeur absolue
+*
+*@int n: Le caractere a verifier
+*Return: n si le caractere est positif et -n
+*si il est negatif
+*
+*/
+
+int _abs(int)
+{
+	if (n > 0)
+{
+	return (n);
+}
+	else
+{
+	return (-n);
+}
+}
