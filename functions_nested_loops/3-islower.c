09e3b319e33bb009b4286dceff32ec364f7025df
Etiennepev <etienneloichot@gmail.com>
2024-10-23 09:14:12 +0000

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - Vérifie si un caractère est en minuscule
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si le caractère est en minuscule, 0 sinon
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
