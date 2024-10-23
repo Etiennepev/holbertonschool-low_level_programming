9fcf8dfa3404c0043e33ecc56f8d7a5afeba78fc
Etiennepev <etienneloichot@gmail.com>
2024-10-23 14:00:26 +0000

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* jack_bauer - Affiche l'alphabet en lettres minuscules dix fois
+*
+*
+*/
+
+void jack_bauer(void)
+{
+	int heures, minutes, a, b, c, d
+
+for (heures = 0; heures < 23; heures++)
+{
+for (minutes = 0; minutes < 24; minutes++)
+	{
+int a = (heures / 10);
+int b = (heures % 10);
+int c = (minutes / 10);
+int d = (minutes % 10);
+{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(':')
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+}
+}
+}
