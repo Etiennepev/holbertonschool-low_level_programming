e92b7185a29a9a84d8924133ae22f6605d88361c
Etiennepev <etienneloichot@gmail.com>
2024-10-23 08:08:03 +0000

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * print_alphabet - Affiche l'alphabet en lettres minuscules
- * Cette fonction affiche toutes les lettres de
- * l'alphabet en lettre minuscules suivies d'un saut de ligne.
- *
- * Return: Nothing
- */
+* print_alphabet - Affiche l'alphabet en lettres minuscules
+*
+* Return: Nothing
+*/
 
 void print_alphabet(void)
 {
