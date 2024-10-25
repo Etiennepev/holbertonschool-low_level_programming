34b19938c398a0b9d06668960a8ee3090083f08d
Etiennepev <etienneloichot@gmail.com>
2024-10-25 11:51:30 +0000

    11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
 * print_to_98 - Affiche l'alphabet en lettres minuscules dix fois
@@ -8,21 +8,21 @@
 
 void print_to_98(int n)
 {
-	if (n < 99)
+	if (n < 98)	
 	{
-	if (n == 98)
-	{
-		printf("%d\n", n);
-	}
 	for (n = n; n < 99; n++)
 	{
 		if (n != 98)
-		{
+	{
 		printf("%d, ", n);
 	}
+		 else
+        {
+                printf("%d\n", n);
 	}
 	}
-	else if (n > 97)
+	
+	else if (n > 98)
 	{
 		for (n = n; n > 97; n--)
 	{
@@ -30,10 +30,11 @@ void print_to_98(int n)
 	{
 		printf("%d, ", n);
 	}
-	}
-	}
 	else
 	{
 		printf("%d\n", n);
 	}
+}
+}
+}
 }
