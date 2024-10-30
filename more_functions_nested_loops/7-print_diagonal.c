1abb51b9755980cb9bd6ac9ed6dcae6331617739
Etiennepev <etienneloichot@gmail.com>
2024-10-30 17:50:05 +0000

    7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,19 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	if (n > 0)
+if (n <= 0)
 	{
-		for (a = 0; a < n; a++)
-		{
-			for (b = 1; b <= a; b++)
-			{
-				if (b != a)
-					_putchar(' ');
-				else if (b == a)
-				{
-				_putchar('\\');
-				_putchar('\n');
-				}
-			}
-		}
-	}
-	else
 	_putchar('\n');
+	return;
+	}
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
 }
