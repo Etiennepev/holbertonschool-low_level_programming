17f436bddc3b28099fd433aa74f3abac02b26a32
Etiennepev <etienneloichot@gmail.com>
2024-10-30 16:50:53 +0000

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - Prints lines
+ *
+ *@n: number of underscore
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		for (; a < n; a++)
+		{
+		_putchar('\');
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
