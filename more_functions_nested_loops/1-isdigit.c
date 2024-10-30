0aa985c5d5185ca16eaafaed71dc1ed9840ecbd5
Etiennepev <etienneloichot@gmail.com>
2024-10-30 14:11:38 +0000

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - check for the uppercase
+ * @c: The character to print
+ *
+ * Return: 1 if uppercase else 0
+ *
+ */
+int _isdigit(int c)
+{
+	if (c > 48 && c < 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+}
