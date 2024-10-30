c92af7dc9281e8f26870d19db43c5ca005b3a90d
Etiennepev <etienneloichot@gmail.com>
2024-10-30 15:27:24 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ *
+ */
+void more_numbers(void)
+{
+	char n;
+	char num;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+		_putchar(num);
+		}
+	_putchar('\n');
+}
+}
