23a9054f79ba1ac4e4f2fd22d85335a4330ba17b
Etiennepev <etienneloichot@gmail.com>
2024-10-30 15:44:29 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,15 @@ void more_numbers(void)
 	int n;
 	int num;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
-			{
-				_putchar(num / 10);
-				_putchar(num % 10);
-			}
+			
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
 		}
+	}
 	_putchar('\n');
-}
 }
