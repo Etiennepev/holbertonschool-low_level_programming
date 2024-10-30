a87952d14253f2f11d8c59b3bbf39a5a9cce86de
Etiennepev <etienneloichot@gmail.com>
2024-10-30 14:45:41 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ * @n: The character to print
+ *
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
