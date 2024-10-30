2da58c670c5b23747f16b08cdcbb428b6e98706a
Etiennepev <etienneloichot@gmail.com>
2024-10-30 14:23:51 +0000

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a, b: The character to print
+ *
+ * Return: 1 if uppercase else 0
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
