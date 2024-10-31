a8bcd7a4edaa5d9bc541d13a5be09141f43f2f7f
Etiennepev <etienneloichot@gmail.com>
2024-10-31 08:55:14 +0000

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - Print diagonal
+ *
+ *@n: number of diagonal
+ */
+void print_square(int size)
+{
+	int height, width;
+	size = 
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (height = 0; height < size; a++)
+	{
+		for (width = 0; width < height; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('/n')
