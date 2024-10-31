be3e318fd3e75b3b2a6c38a8ee3cad8babdd9ddf
Etiennepev <etienneloichot@gmail.com>
2024-10-31 09:43:39 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 /**
  *
@@ -7,8 +6,7 @@
  */
 int main (void)
 {
-	int i = 1;
-	for (; i <= 100; i++)
+	for (int i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
