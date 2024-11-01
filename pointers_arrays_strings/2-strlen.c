3ea7e4733d2f8130688e09d990759fd8658cdc47
Etiennepev <etienneloichot@gmail.com>
2024-11-01 12:55:32 +0000

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - lenght of a strig
+ *
+ *
+ *@s: l'endroit ou est la chaine de caracteres
+ *Return: 1 or 0
+ */
+
+int _strlen(char *s)
+{
+	char str[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+}
