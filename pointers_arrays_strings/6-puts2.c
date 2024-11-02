38a1bde33010033ea43f090185b56cb118724a1f
Etiennepev <etienneloichot@gmail.com>
2024-11-02 09:46:58 +0000

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print every 2 numbers
+ *
+ *
+ *@str: print 2468
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+}
