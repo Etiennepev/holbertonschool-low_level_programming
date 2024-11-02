8922d6e7221ca9c4c9667550b8b568d01b6ba207
Etiennepev <etienneloichot@gmail.com>
2024-11-02 10:05:42 +0000

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,14 +10,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str [i + 2] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
-	}
-	if (str[i] == '\0')
-	{
-		break;
 	}
 	_putchar('\n');
 }
