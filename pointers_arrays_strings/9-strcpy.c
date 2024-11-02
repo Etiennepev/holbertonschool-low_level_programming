62f67ad4cd82db782c5ab66c0a0627f86cdcba1e
Etiennepev <etienneloichot@gmail.com>
2024-11-02 14:21:46 +0000

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+/**
+ * *_strcpy - copy a string
+ *
+ * @dest: the copy have to be there
+ * @src: the original
+ *
+ * Return: the dest result
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+
+	return (*dest);
