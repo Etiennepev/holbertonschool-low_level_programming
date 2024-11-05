cd2d1b85abffd7fbdea29b30340ca75bd8fd02d0
Etiennepev <etienneloichot@gmail.com>
2024-11-05 13:49:24 +0000

    3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+ #include "main.h"
+/**
+ * *_strspn - count specific caractere
+ *
+ * @s: the string to analyse
+ * @accept: the string with correct words
+ *
+ * Return: the caractere of s who are in accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i, j;
+	int found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found++;
+				break;
+			}
+		}
+		count++;
+	}
+	return (count);
+}
