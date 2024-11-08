3b498d428ae5270b5d0d242f6475a21ddd60edbc
Etiennepev <etienneloichot@gmail.com>
2024-11-08 11:28:53 +0000

    6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * check_primen - 
+ *
+ * @n: perfect
+ * Return: good
+ */
+
+int check_primen (int n, int y)
+{
+	if (n >= y)
+	{
+	if (n % y == 0)
+	{
+		return (0);
+	}
+	if (y * y > n)
+	{
+		return (1);
+	}
+	else if ( n % y != 0)
+	{
+		return (check_primen(n, y + 1));
+	}
+	}
+	return (0);
+}
+
+/**
+ * is_prime_number -
+ *
+ *@n: yes
+ * Return: good
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+	return (1);
+	}
+	else if	(n > 2)
+	{
+		int y = 2;
+
+		return (check_primen(n, y + 1));
+	}
+	return (0);
+}
