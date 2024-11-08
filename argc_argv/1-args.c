275c0ff5d64144a6e44031cc7a72fda605d980d1
Etiennepev <etienneloichot@gmail.com>
2024-11-08 13:38:07 +0000

    print a number of arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print a name of the program
+ *
+ * @argv: array containing the arg
+ * @argc: the number of arg
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
