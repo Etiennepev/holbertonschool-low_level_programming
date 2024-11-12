48f01f54de518055bd7a80f910cbfef1a52a0e3e
Etiennepev <etienneloichot@gmail.com>
2024-11-12 13:23:21 +0000

    2 array

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+/**
+ *  alloc_grid-  concatenates two strings.
+ *
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ *
+ * Return: a pointer to the string or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0;
+	int **array;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	array = (int **)malloc(height * sizeof(int *));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (; i < height; i++)
+	{
+	array[i] = (int *)malloc(width * sizeof(int));
+
+	if (array[i] == NULL)
+	{
+	for (; j < i; j++)
+	{
+	free(array[j]);
+	}
+	free(array);
+	return (NULL);
+	}
+	for (; j < width; j++)
+	{
+	array[i][j] = 0;
+	}
+	}
+	return (array);
+}
