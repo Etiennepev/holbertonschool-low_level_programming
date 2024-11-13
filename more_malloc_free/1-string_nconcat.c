2d62c024e4b175af55efe5c578fd21a080538253
Etiennepev <etienneloichot@gmail.com>
2024-11-13 09:41:19 +0000

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,14 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 != 0)
 	{
 	}
-	for (; s1[size1] != '\0'; size1++)
+	while (s1[size1] != '\0')
 	{
+		size1++;
 	}
 	if (s2 != 0)
 	{
 	}
-	for (; s2[size2] != '\0' && size2 < n; size2++)
+	while (s2[size2] != '\0' && size2 < n)
 	{
+		size2++;
 	}
 	newstr = (char *)malloc((size1 + size2 + 1) * sizeof(char));
 
