497b18ec8a28957d2dcf512be8e73aa83bf1b5b5
Etiennepev <etienneloichot@gmail.com>
2024-11-13 14:05:46 +0000

    dog life

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * init_dog - Initialize a dog structure with specific values
+ * @d: Pointer to the struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
