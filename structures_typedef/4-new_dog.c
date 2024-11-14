cb4853ce65cefeded6ff071300e3d3f0ead082d5
Etiennepev <etienneloichot@gmail.com>
2024-11-14 12:38:12 +0000

    create a new puppy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,47 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _strlen - returns the length of a string.
+ * @s: string to analyze.
+ * Return: lenght of s.
+ **/
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0;  s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
+
+/**
+ * _strncpy - copies a string.
+ * @dest: string of destinatation.
+ * @src : string to copy.
+ * @n: bite limit.
+ * Return: dest.
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/**
  *new_dog - Creates a new dog and returns a pointer to the new dog structure
  * @name: The name of the dog
  * @age: The age of the dog
