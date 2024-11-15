1ddf4180998c8190fcac39d91692a78a8b479ad4
Etiennepev <etienneloichot@gmail.com>
2024-11-15 09:15:25 +0000

    free the little doggo

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *  free_dog - Prints the details of a struct dog
+ * @d: pointer
+ *
+ * Return: Nothing
+ **/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		return;
+		free(d->name);
+	}
+	if (d->owner != NULL)
+	{
+		return;
+		free(d->owner);
+		free(d);
+	}
+}
