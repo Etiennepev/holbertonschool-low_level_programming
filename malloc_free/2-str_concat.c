df26460938d3439d4221102aa150f310c326dfe6
Etiennepev <etienneloichot@gmail.com>
2024-11-15 13:00:33 +0000

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,10 +2,8 @@
 
 /**
  * str_concat -  concatenates two strings.
- *
  * @s1: the original
  * @s2: the one whos gonna be concatenates
- *
  * Return: a pointer to the string or NULL
  */
 
@@ -14,20 +12,19 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, size1 = 0, size2 = 0;
 	char *newstr;
 
-	if (s1 == 0)
+	if (s1 != NULL)
 	{
-		size1 = 0;
+		for (; s1[size1] != '\0'; size1++)
+		{
+		}
 	}
-	for (; s1[size1] != '\0'; size1++)
+	if (s2 != NULL)
 	{
+		for (; s2[size2] != '\0'; size2++)
+		{
+		}
 	}
-	if (s2 == 0)
-	{
-		size2 = 0;
-	}
-	for (; s2[size2] != '\0'; size2++)
-	{
-	}
+
 	newstr = (char *)malloc((size1 + size2 + 1) * sizeof(char));
 
 	if (newstr ==  NULL)
@@ -35,11 +32,10 @@ char *str_concat(char *s1, char *s2)
 	return (NULL);
 	}
 	else
-
-	for (; i < size1; i++)
-	{
-		newstr[i] = s1[i];
-	}
+		for (; i < size1; i++)
+		{
+			newstr[i] = s1[i];
+		}
 	for (; j < size2; j++)
 	{
 		newstr[i + j] = s2[j];
