ec2588c85ce6bb0c51ab371aa237c2cd0e24086a
Etiennepev <etienneloichot@gmail.com>
2024-11-18 10:54:20 +0100

    Call a function by a pointer

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
-/*
-* print_name - a fonction to print a name
-* @name: Pointer to an array
-* @f: pointer to the function to print a name
-* Return: Nothing
-*/
+/**
+ * print_name- a fonction to print a name
+ * @name: Pointer to an array
+ * @f: pointer to the function to print a name
+ * Return: Nothing
+ **/
 
 void print_name(char *name, void (*f)(char *))
 {
