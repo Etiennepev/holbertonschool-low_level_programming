e67689632b1dce5bdcc67bfd80a4cf8177bc7d27
Etiennepev <etienneloichot@gmail.com>
2024-11-18 15:08:45 +0100

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
+
 
 /**
  * print_name- a fonction to print a name
  * @name: Pointer to an array
  * @f: pointer to the function to print a name
- **/
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
