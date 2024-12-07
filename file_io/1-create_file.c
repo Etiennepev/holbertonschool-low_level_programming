bcf3501757c31ccd4c529ca27e08a7529ae5bf98
Etiennepev <etienneloichot@gmail.com>
2024-12-07 13:53:51 +0100

    trying to fix some errors

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	byteswrite = write(1, text_content, strlen(text_content));
+	byteswrite = write(fd, text_content, strlen(text_content));
 	if (byteswrite == -1)
 	{
 		close(fd);
